72d09126707da9c2fa9ef3abf016b87d64604163
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-10-25 20:36:01 +0530

    UC3 added

--- src/TicTacToe1.java
+++ src/TicTacToe1.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class TicTacToe1 {
 	
 	char[] boardOfChar = new char[10];
-	
+	char playerChar;
+	char systemChar;
 	void  boardOfCharacter() {
 		
 		boardOfChar[1] = ' ';
@@ -21,18 +22,21 @@ public class TicTacToe1 {
 	}
 	void playerInput_UC1_(){
 		Scanner inp=new Scanner(System.in);
+		System.out.println("Choose X Or O");
 		 char c = inp.next().charAt(0);
-		 char playerChar;
-		 char systemChar;
+		
 		 if(c=='O') {
 			 System.out.println("Player has choosed O");
 			 playerChar='O';
 			 systemChar='X';
 		 }
-		 else {
+		 else if(c=='X') {
 			 System.out.println("Player has Choosed X");
 			 playerChar='X';
 			 systemChar='O';
+		 }
+		 else {
+			 System.out.println("Wrong Choice ");
 		 }
 	}
 	 public void printBoard() {
@@ -56,10 +60,113 @@ public class TicTacToe1 {
 		         }
 		 
 		     }
+	 public void moveOfPlayer() {
+		 Scanner inp=new Scanner(System.in);
+		 System.out.println("Enter ther desired position in Game (1-9 ");
+		 int choice=inp.nextInt();
+		switch (choice){
+		case 1:
+			if (choice==1 && playerChar=='O' )
+		{
+			boardOfChar[1]='O';
+		}
+			else if(choice == 2 && playerChar=='X')
+			{
+				boardOfChar[1]='x';
+			}
+			break;
+			
+		case 2:
+			if (choice==2 && playerChar=='O' )
+				{
+				boardOfChar[2]='O';
+				}
+				else if(choice == 2 && playerChar=='X')
+				{
+					boardOfChar[2]='x';
+				}
+				break;
+				
+		case 3:
+			if (choice==3 && playerChar=='O' )
+				{
+				boardOfChar[3]='O';
+				}
+				else if(choice == 3 && playerChar=='X')
+				{
+					boardOfChar[3]='x';
+				}
+				break;
+		case 4:
+			if (choice==4 && playerChar=='O' )
+				{
+				boardOfChar[4]='O';
+				}
+			else if(choice == 2 && playerChar=='X')
+				{
+					boardOfChar[4]='x';
+				}
+			break;
+		case 5:
+			if (choice==2 && playerChar=='O' )
+				{
+				boardOfChar[5]='O';
+				}
+			else if(choice == 2 && playerChar=='X')
+				{
+					boardOfChar[5]='x';
+				}
+			break;
+		case 6:
+			if (choice==2 && playerChar=='O' )
+				{
+				boardOfChar[6]='O';
+				}
+			else if(choice == 2 && playerChar=='X')
+				{
+					boardOfChar[6]='x';
+				}
+			break;
+		case 7:
+			if (choice==2 && playerChar=='O' )
+			 	{
+				boardOfChar[7]='O';
+			 	}
+			else if(choice == 2 && playerChar=='X')
+				{
+					boardOfChar[7]='x';
+				}
+			break;
+		case 8:
+			if (choice==2 && playerChar=='O' )
+				{
+				boardOfChar[8]='O';
+				}
+			else if(choice == 2 && playerChar=='X')
+			{
+					boardOfChar[8]='x';
+			}
+			break;
+		case 9:
+			if (choice==2 && playerChar=='O' )
+				{
+				boardOfChar[9]='O';
+				}
+			else if(choice == 2 && playerChar=='X')
+				{
+					boardOfChar[9]='x';
+				}
+			break;
+			default:
+				System.out.println("Wrong Choice");
+		}
+	 }
 
 	public static void main(String[] args) {
 	    TicTacToe1 rv=new TicTacToe1();
 	    rv.boardOfCharacter();
+	    rv.playerInput_UC1_();
+	    rv.moveOfPlayer();
 	  rv.printBoard();
 		
 	}
